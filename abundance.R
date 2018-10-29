8e6e34f94c523072efe5f8402764a9096e3a2fff
Lisa-Monique Edward <ledwar29@uwo.ca>
2018-10-29 11:19:44 -0400

    documentation

--- abundance.R
+++ abundance.R
@@ -1,11 +1,16 @@
 seq_abundance <- function(seqs){
+  # @arg seqs: list of sequences
+  # @return environment of (sequence: abunance) key:value pairs
+  
   abundance <- new.env()
   for (s in seqs){
     sequence <- s$sequence
     if (exists(sequence, envir = abundance)){
+      # if sequence already in structure, increment count
       abundance[[sequence]] <-abundance[[sequence]] + 1 
     }
     else{
+      # otherwise, add sequence and initiate count
       abundance[[sequence]] <- 0
     }
   }
