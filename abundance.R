8097448e2dc01a77d2623518359a10365535740c
Lisa-Monique Edward <ledwar29@uwo.ca>
2018-11-05 12:36:26 -0500

    compression function as s3 method

--- abundance.R
+++ abundance.R
@@ -1,19 +1,54 @@
-seq_abundance <- function(seqs){
-  # @arg seqs: list of sequences
-  # @return environment of (sequence: abunance) key:value pairs
+compress <- function(x){
+  UseMethod("compress", x)
+}
+
+
+compress.session <- function(session) {
+  # @arg session: environment containing imported and parsed sequence Data objects
+  # populates each Data object's compressed environment with calculated sequence abundances
   
-  abundance <- new.env()
-  for (s in seqs){
-    sequence <- s$sequence
-    if (exists(sequence, envir = abundance)){
-      # if sequence already in structure, increment count
-      abundance[[sequence]] <-abundance[[sequence]] + 1 
-    }
-    else{
-      # otherwise, add sequence and initiate count
-      abundance[[sequence]] <- 0
-    }
+  # validate session contains imported sequences
+  if (length(ls(session))==0) {
+    stop("Error: session empty. Run highlineR::import(...)")
   }
   
-  abundance
+  # calculate sequence abundance for each Data object
+  for (data in ls(session)) {
+    compress(get(data, envir = session, inherits = FALSE))
+  }
+}
+
+
+compress.Data <- function(data) {
+  # @arg data: Data object containing parsed list of sequences
+  # populates Data object's compressed environment with (sequence: abunance) key:value pairs
+  
+  # ignore unparsed files
+  if (length(data$raw_seq) == 0) {
+    warning(paste("File", get(data, envir = session)$path, "ignored. Run highlineR::parse(...)"))
+  }
+  # ignore already compressed files
+  else if(length(data$compressed) != 0) {
+    warning(paste("File", get(data, envir = session)$path, "ignored. Already compressed."))
+  }
+  else {
+    for (s in data$raw_seq){
+      sequence <- s$sequence
+      if (exists(sequence, envir = data$compressed)){
+        # if sequence already in structure, increment count
+        data$compressed[[sequence]] <-data$compressed[[sequence]] + 1 
+      }
+      else{
+        # otherwise, add sequence and initiate count
+        data$compressed[[sequence]] <- 0
+      }
+    }
+  }
+}
+
+
+compress.default <- function(x){
+  warning(paste("Error: highlineR cannot compress objects of class ",
+                class(x),
+                "and can only be used on sessions or Data objects."))
 }
