c6eb7a8983519281e22d70fceb5d9b37498d0601
Lisa-Monique Edward <ledwar29@uwo.ca>
2018-11-15 16:45:38 -0500

    formal summary and print methods

--- plot.R
+++ plot.R
@@ -1,13 +1,69 @@
-data <- highlineR.data$`/home/lisamonique/Documents/highlineR/data/test.fa`
-
-summary.Data <- function(data) {
-  cat("Summary:\n")
-  cat("Data type:", class(data)[1], "\n")
-  cat("Number of sequences:", length(data$raw_seq), "\n")
-  cat("Sequence length:", nchar(data$raw_seq[[1]]$sequence), "\n")
-  cat("Number of variants:", length(ls(data$compressed)), "\n\n")
+summary.Data <- function(data, ...) {
+  # @arg session highlineR Data object to be summarized
+  # @return list of summarized variables
   
-  ls.str(data$compressed)
+  fn <- data$path # filename
+  dt <- class(data)[1] # datatype of file
+  seq_num <- length(data$raw_seq) # number of sequences
+  seq_len <- nchar(data$raw_seq[[1]]$sequence) # length of sequence
+  var_num <- length(ls(data$compressed)) # number of variants
+  
+  # convert environment of variant counts to matrix
+  var_counts <- (as.data.frame(as.list(data$compressed))) 
+  # order variant matrix by abundance
+  var_counts.sorted <- t(var_counts[order(var_counts, decreasing = TRUE)])
+  colnames(var_counts.sorted) <- "freq"
+  
+  abun_var <- rownames(var_counts.sorted)[1] # most abundant variant
+  
+  res <- list(fn = fn,
+              dt = dt,
+              seq_num = seq_num,
+              seq_len = seq_len,
+              var_num = var_num,
+              abun_var = abun_var,
+              vars = var_counts.sorted)
+  class(res) <- "summary.Data"
+  res
+}
+
+print.summary.Data <- function(x, ...) {
+  cat("File: ")
+  cat(x$fn)
+  
+  cat("\nDatatype: ")
+  cat(x$dt)
+  
+  cat("\nNumber of Sequences: ")
+  cat(x$seq_num)
+  
+  cat("\nSequence Length: ")
+  cat(x$seq_len)
+  
+  cat("\nNumber of Variants: ")
+  cat(x$var_num)
+  
+  cat("\nMost Abundant Variant: ")
+  cat(x$abun_var)
+  cat("\n\nVariants:\n")
+  
+  print(x$vars)
+  
+  cat("\n")
+  cat(rep("_", 40))
+  cat("\n\n")
+}
+
+summary.session <- function(session, ...) {
+  res <- eapply(session, summary)
+  class(res) <- "summary.session"
+  res
+}
+
+print.summary.session <- function(x, ...) {
+  for (i in 1:length(x)){
+    print(x[[i]])
+  }
 }
 
 plot.session <- function(session = highlineR.data, master) {
