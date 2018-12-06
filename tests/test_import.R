c1f198f1d68743d36168cfd59e37148205293566
Lisa-Monique Edward <ledwar29@uwo.ca>
2018-12-06 18:08:16 -0500

    testthat package

--- tests/test_import.R
+++ tests/test_import.R
@@ -1,65 +1,166 @@
-require(RUnit, quietly=TRUE)
+require(testthat, quietly=TRUE)
+setwd("/home/lisamonique/Documents/highlineR")
 source("import.R")
+base <- "/home/lisamonique/Documents/highlineR/tests/test_data/"
 
-test.import <- function() {
-  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/"
-  test <- paste0(base, "test.fa")
+
+test_that("invalid files do not open", {
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
   
-  # non existent file
-  checkException(import(paste0(base, "test.fq")), "Error: file /home/lisamonique/Documents/highlineR/data/test.fq not found")
-  # file with invalid extension
-  checkException(import(paste0(base, "test.txt")), "Error: highlineR does not know how to handle files of type txt and can only be used on fasta and fastq files")
-  # file with valid extension, invalid file type forced
-  checkException(import(test, datatype = "txt"), "Error: highlineR does not know how to handle files of type txt and can only be used on fasta and fastq files")
+  # non-existent file
+  expect_error(import_raw_seq(paste0(base, "test.fq"), session = "test_sess"),
+               paste0("ERROR: path '", paste0(base, "test.fq", "' not valid")))
+  # file with invalid extension, default import
+  expect_error(import_raw_seq(paste0(base, "invalid/test.txt"), session = "test_sess"),
+               paste0("ERROR: file '", paste0(base, "invalid/test.txt", "' not imported. highlineR does not know how to handle files of type txt and can only be used on fasta and fastq files")))
+  # file with valid extension, specify import with invalid extension
+  expect_error(import_raw_seq(paste0(base, "valid/fasta/test.fa"), datatype = "txt", session = "test_sess"),
+               paste0("ERROR: file '", paste0(base, "valid/fasta/test.fa", "' not imported. highlineR does not know how to handle files of type txt and can only be used on fasta and fastq files")))
   
-  
+})
+
+
+test_that("highlineR s3 Data objects created correctly", {
   expected_Data <- structure(
     as.environment(
       list(
-        path = test, 
+        path = paste0(base, "valid/fasta/test.fa"), 
         raw_seq = list(), 
-        compressed = new.env()
+        compressed = 
+          structure(
+            new.env(),
+            class = c("compressed", "environment")
+          ),
+        master = character(),
+        seq_diff = matrix()
       )
     ), 
     class = c("fasta", "Data", "environment")
   )
-  checkEquals(Data(test), expected_Data)
+  parent.env(expected_Data$compressed) <- expected_Data
   
+  # create Data object
+  expect_error(result <- Data(paste0(base, "valid/fasta/test.fa")),
+               NA)
+  # check correct structure with correct class
+  expect_equal(expected_Data, result)
+  expect_s3_class(result, "Data")
+})
+
+
+test_that("sessions initate correctly with or without file import", {
+  test_filename <- paste0(base, "valid/fasta/test.fa")
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
+  if (exists("test_sess_2", envir = .GlobalEnv)){
+    rm(test_sess_2, envir = .GlobalEnv)
+  }
+  
+  # empty session structure
   expected_session <- structure(new.env(), class = c("session", "environment"))
   
   # initialise empty session
-  result <- init("test_sess")
-  checkEquals(expected_session, result)
+  result <- init_session("test_sess")
+  expect_equal(expected_session, result)
   
-  assign(test, expected_Data, envir = expected_session)
-
+  # session structure with single file
+  expected_Data <- structure(
+    as.environment(
+      list(
+        path = test_filename, 
+        raw_seq = list(), 
+        compressed = 
+          structure(
+            new.env(),
+            class = c("compressed", "environment")
+          ),
+        master = character(),
+        seq_diff = matrix()
+      )
+    ), 
+    class = c("fasta", "Data", "environment")
+  )
+  parent.env(expected_Data$compressed) <- expected_Data
+  assign(test_filename, expected_Data, envir = expected_session)
+  
   # import file to already initialised, empty session
-  checkEquals(expected_session, import(test, session = "test_sess"))
-  
-  if (exists("test_sess")){
-    rm(test_sess)
+  expect_equal(expected_session, import_raw_seq(test_filename, session = "test_sess"))
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
   }
   
   # import file to uninitalised session
-  checkEquals(expected_session, import(test, session = "test_sess"))
+  expect_equal(expected_session, import_raw_seq(test_filename, session = "test_sess_2"))
   
-  # import already imported file, expect warning, no change
-  checkEquals(expected_session, import(test, session = "test_sess"))
+  # import already imported file
+  expect_warning(import_raw_seq(test_filename, session = "test_sess_2"),
+                 "File /home/lisamonique/Documents/highlineR/tests/test_data/valid/fasta/test.fa ignored. Already imported in test_sess_2 session")
+  expect_equal(expected_session, test_sess_2) # no new file added
   
-  assign(paste0(base, "test2.fa"), Data(paste0(base, "test2.fa")), envir = expected_session)
+  # import already imported file, forced
+  expect_warning(import_raw_seq(test_filename, session = "test_sess_2", force = TRUE),
+                 NA)
+  if (exists("test_sess_2", envir = .GlobalEnv)){
+    rm(test_sess_2, envir = .GlobalEnv)
+  }
+  
+  assign(paste0(base, "valid/fasta/test2.fa"), Data(paste0(base, "valid/fasta/test2.fa")), envir = expected_session)
   
   # import directory
-  checkEquals(expected_session, import(base, session = "test_sess"))
-}
-
-test.parser <- function() {
-  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/"
-  init("test_sess")
-  expected_session <- import(base, session = "test_sess")
+  expect_error(import_raw_seq(paste0(base, "valid/fasta/"), session = "test_sess_2"),
+                 NA)
+  expect_equal(expected_session, test_sess_2)
   
-  # parse fasta file
-  # parse(expected_session$`/home/lisamonique/Documents/highlineR/tests/test_data/test.fa`)
-  # parse fastq file
-  # parse already parsed file
-  # parse entire session
-}
+  if (exists("test_sess_2", envir = .GlobalEnv)){
+    rm(test_sess_2, envir = .GlobalEnv)
+  }
+})
+
+test_that("sessions can be initialised and closed", {
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
+  
+  # initiate session
+  expect_error(init_session("test_sess"),
+               NA)
+  # check if session exists and is of class session
+  expect_true(exists("test_sess", envir = .GlobalEnv))
+  expect_s3_class(test_sess, "session")
+  
+  # close session
+  expect_error(close_session(test_sess),
+               NA)
+  # check that session no longer exists
+  expect_false(exists("test_sess", envir = .GlobalEnv))
+  
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
+})
+
+test_that("Data objects can be removed from sessions", {
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
+  test_filename <- paste0(base, "valid/fasta/test.fa")
+  
+  # initiate session with test file
+  import_raw_seq(test_filename, session = "test_sess")
+  
+  # check that file exists in session
+  expect_true(exists(test_filename, envir = test_sess))
+  
+  # remove file from session
+  expect_error(remove_Data(test_filename, session = "test_sess"),
+               NA)
+  # check that file no longer exists in session
+  expect_false(exists(test_filename, envir = test_sess))
+  
+  if (exists("test_sess", envir = .GlobalEnv)){
+    rm(test_sess, envir = .GlobalEnv)
+  }
+})
---
+++ tests/test_parser.R
@@ -0,0 +1,114 @@
+require(testthat, quietly=TRUE)
+setwd("/home/lisamonique/Documents/highlineR")
+source("import.R")
+source("parser.R")
+
+test_that("fasta files parsed correctly", {
+  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/valid/fasta/"
+  test_filename <- paste0(base, "test.fa")
+  
+  # parse valid fasta data object
+  test_Data <- Data(test_filename)
+  parse(test_Data)
+  
+  expected_raw_seq <- lapply(
+    list(
+      list("adam", "ACTGACTAGCTAGCTAACTG"),
+      list("bob", "GCATCGTAGCTAGCTACGAT"),
+      list("courtney", "CATCGATCGTACGTACGTAG"),
+      list("danny", "ATCGATCGATCGTACGATCG"),
+      list("john", "ACTGACTAGCTAGCTAACTG"),
+      list("sam", "ACTGACTAGCTAGCTAACTG")
+    ),
+    setNames,
+    c("header", "sequence")
+    )
+  
+  expect_equal(test_Data$raw_seq, expected_raw_seq)
+  
+  test_filename <- paste0(base, "test2.fa")
+  # parse valid fasta data object with multi-line sequences
+  test_Data <- Data(test_filename)
+  expect_error(parse(test_Data),
+               NA)
+  expect_equal(test_Data$raw_seq, expected_raw_seq)
+})
+
+test_that("quality score conversion correct", {
+  # default = sanger
+  expect_equal(convert_quality(intToUtf8(33:73)),
+               0:40)
+  expect_equal(convert_quality(intToUtf8(33:73), encoding = "sanger"),
+               0:40)
+  expect_equal(convert_quality(intToUtf8(59:104), encoding = "solexa"),
+               -5:40)
+  expect_equal(convert_quality(intToUtf8(64:104), encoding = "illumina1.3"),
+               0:40)
+  expect_equal(convert_quality(intToUtf8(67:104), encoding = "illumina1.5"),
+               3:40)
+  expect_equal(convert_quality(intToUtf8(33:74), encoding = "illumina1.8"),
+               0:41)
+  # invalid quality scoring encoding
+  expect_error(convert_quality(intToUtf8(33:73), encoding = "invalid"),
+               "'arg' should be one of “sanger”, “solexa”, “illumina1.3”, “illumina1.5”, “illumina1.8”")
+})
+
+test_that("fastq files parsed correctly", {
+  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/valid/fastq/"
+  test_filename <- paste0(base, "test.fq")
+  
+  # parse valid fastq data object
+  test_Data <- Data(test_filename)
+  expect_error(parse(test_Data),
+               NA)
+  
+  expected_raw_seq <- lapply(
+    list(
+      list("adam", "ACTGACTAGCTAGCTAACTG", convert_quality("9C;=;=<9@4868>9:67AA")),
+      list("bob", "GCATCGTAGCTAGCTACGAT", convert_quality("1/04.72,(003,-2-22+0")),
+      list("courtney", "CATCGATCGTACGTACGTAG", convert_quality("?7?AEEC@>=1?A?EEEB9E")),
+      list("danny", "ATCGATCGATCGTACGATCG", convert_quality(">=2.660/?:36AD;0<147")),
+      list("john", "ACTGACTAGCTAGCTAACTG", convert_quality("8;;;>DC@DAC=B?C@9?B?")),
+      list("sam", "ACTGACTAGCTAGCTAACTG", convert_quality("-/CA:+<599803./2065?"))
+    ),
+    setNames,
+    c("header", "sequence", "quality")
+  )
+  
+  expect_equal(test_Data$raw_seq, expected_raw_seq)
+})
+
+test_that("invalid files not parsed", {
+  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/valid/"
+  
+  # import fasta format file as fastq and parse
+  test_filename <- paste0(base, "fasta/test.fa")
+  test_Data <- Data(test_filename, datatype = "fq")
+  expect_error(parse(test_Data), "ERROR: Failed to parse FASTQ at line: 0")
+  
+  # import fasta format file as fastq and parse
+  test_filename <- paste0(base, "fastq/test.fq")
+  test_Data <- Data(test_filename, datatype = "fa")
+  expect_error(parse(test_Data), "ERROR: Failed to parse FASTA at line: 0")
+  
+  # try to parse already parsed file
+  test_filename <- paste0(base, "fasta/test.fa")
+  test_Data <- Data(test_filename)
+  parse(test_Data)
+  expect_warning(parse(test_Data), "ERROR: file '/home/lisamonique/Documents/highlineR/tests/test_data/valid/fasta/test.fa' ignored. Already parsed.")
+})
+
+test_that("directories can be parsed", {
+  base <- "/home/lisamonique/Documents/highlineR/tests/test_data/valid/"
+  if (exists("test_sess")){
+    rm (test_sess, envir = .GlobalEnv)
+  }
+  
+  test <- import_raw_seq(base, session = "test_sess")
+  expect_error(parse(test), NA)
+  expect_setequal(ls(test), c(paste0(base, "fasta/test.fa"), paste0(base, "fastq/test.fq"), paste0(base, "fasta/test2.fa")))
+  
+  if (exists("test_sess")){
+    rm (test_sess, envir = .GlobalEnv)
+  }
+})
