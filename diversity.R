e19d0c148a3af0a3a9fdb814b61640280606a08f
Lisa-Monique Edward <ledwar29@uwo.ca>
2019-02-25 13:19:55 -0500

    corrected Shannon Entropy formula, calculate diversity measures on sample

--- diversity.R
+++ diversity.R
@@ -1,13 +1,16 @@
+library(ape)
+library(parallel)
+
 calc_Diversity <- function(x) {
   UseMethod("calc_Diversity", x)
 }
 calc_Diversity.session <- function(session) {
   n <- ls(session)
   names(n) <- ls(session)
-  res <- mclapply(n, function(data) calc_Diversity(session[[data]]))
+  res <- mclapply(n, function(data) calc_Diversity(session[[data]]), mc.cores = 20)
   dt <- data.frame(t(sapply(res,c)))
   colnames(dt) <- c("Shannon Entropy", "Percent Complexity", "Nucleotide Diversity", "Percent Diversity")
-  write.csv(res, file = "diversity.csv")
+  write.csv(dt, file = "diversity.csv")
   dt
 }
 
@@ -17,14 +20,15 @@ calc_Diversity.Data <- function(data) {
 
 Shannon_Entropy <- function(data) {
   H <- 0 # result
-  dt <- t(data.frame(strsplit(ls(data$compressed), ""))) # unique variants as dataframe
+  dt <- t(data.frame(strsplit(ls(data$sample), ""))) # unique variants as dataframe
   rownames(dt) <- NULL
   L <- ncol(dt) # length of sequence
+  n <- nrow(dt) # number of sequences
   
   # calculate entropy at each position
   for (l in 1:L) {
-    v <- as.data.frame(table(dt[, l]))[, 2] # freq of each nucleotide at position l
-    H <- H + -sum(v * log2(v))
+    v <- as.data.frame(table(dt[, l]))[, 2]/n # relative freq of each nucleotide at position l
+    H <- H + -sum(v * log(v))
   }
   H/L
  }
@@ -32,45 +36,47 @@ Shannon_Entropy <- function(data) {
 Percent_Complexity <- function(data) {
   # total number of reads
   N <- 0
-  for (v in ls(data$compressed)) {
-    N <- N + data$compressed[[v]]
+  for (v in ls(data$sample)) {
+    N <- N + data$sample[[v]]
   }
   
   # number of distinct variants divided by the total number of reads x 100
-  length(data$compressed)/N * 100
+  length(data$sample)/N * 100
 }
 
 Nucleotide_Diversity <- function(data) {
   pi <- 0 # result
   
   # number of unique variants
-  n <- length(data$compressed) 
+  n <- length(data$sample) 
   
   # total number of reads
   N <- 0 
-  for (v in ls(data$compressed)) {
-    N <- N + data$compressed[[v]]
+  for (v in ls(data$sample)) {
+    N <- N + data$sample[[v]]
   }
   
   # pairwise comparison of unique variants
-  for (i in 2:n) {
-    for (j in 1:(i-1)) {
-      s1 <- strsplit(ls(data$compressed)[i], "")[[1]]
-      s2 <- strsplit(ls(data$compressed)[j], "")[[1]]
-      
-      # ignored gapped positions
-      gaps <- union(which(s1 == "-"), which(s2 == "-"))
-      
-      # length of sequence after gap removal
-      l <- length(s1[-gaps]) 
-      if (l == 0)
-        next 
-      
-      # number of nucleotide differences per site
-      pi_ij <- (length(which(s1[-gaps] != s2[-gaps])))/l 
-      
-      # sum (frequency of sequence i * frequency of sequence j * pi_ij)
-      pi <- pi + ((data$compressed[[paste(s1, collapse = "")]]/N) * (data$compressed[[paste(s2, collapse = "")]])/N * pi_ij)
+  if (n > 1) {
+    for (i in 2:n) {
+      for (j in 1:(i-1)) {
+        s1 <- strsplit(ls(data$sample)[i], "")[[1]]
+        s2 <- strsplit(ls(data$sample)[j], "")[[1]]
+        
+        # ignored gapped positions
+        gaps <- union(which(s1 == "-"), which(s2 == "-"))
+        
+        # length of sequence after gap removal
+        l <- length(s1[-gaps]) 
+        if (l == 0)
+          next 
+        
+        # number of nucleotide differences per site
+        pi_ij <- (length(which(s1[-gaps] != s2[-gaps])))/l 
+        
+        # sum (frequency of sequence i * frequency of sequence j * pi_ij)
+        pi <- pi + ((data$sample[[paste(s1, collapse = "")]]/N) * (data$sample[[paste(s2, collapse = "")]])/N * pi_ij)
+      }
     }
   }
   2 * pi
@@ -79,8 +85,8 @@ Nucleotide_Diversity <- function(data) {
 Percent_Diversity <- function(data) {
   # TODO: average pairwise genetic distance between two sequences, others used MEGA
   seqs <- vector()
-  for(s in ls(data$compressed)) {
-    seqs <- c(seqs, rep(s, data$compressed[[s]]))
+  for(s in ls(data$sample)) {
+    seqs <- c(seqs, rep(s, data$sample[[s]]))
   }
   y <- t(sapply(strsplit(seqs, split = ""), tolower))
   rownames(y) <-seqs
